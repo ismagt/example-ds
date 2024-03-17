517807a87e9ea497ed9fb8f6ba1c8392223a6cdf
ismagt <47352846+ismagt@users.noreply.github.com>
2024-03-17 19:22:18 +0100

    Update app_pisa_map

--- app_pisa_mapa_3.py
+++ app_pisa_mapa_3.py
@@ -37,7 +37,7 @@ test_indices = X_test.index
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 community_options = [{'label': 'All communities', 'value': 'All'}] + \
                     [{'label': community, 'value': community} for community in df_vis['Community'].unique()]
 
