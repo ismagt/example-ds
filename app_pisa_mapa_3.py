7bd14e191434e1a1edcff7078c61b2396e168391
ismagt <47352846+ismagt@users.noreply.github.com>
2024-04-10 02:31:34 +0200

    Update app_pisa_mapa_3.py

--- app_pisa_mapa_3.py
+++ app_pisa_mapa_3.py
@@ -668,4 +668,4 @@ def update_output(selected_community, clickData):
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8075)
+    app.run_server(debug=False)
